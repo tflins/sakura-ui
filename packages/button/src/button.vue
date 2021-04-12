45530eec7ee334341a361ecb150418bb923ccd1d
tflin <tflins@163.com>
2021-04-13 00:42:16 +0800

    refactor: 引入 sass

--- packages/button/src/button.vue
+++ packages/button/src/button.vue
@@ -1,5 +1,5 @@
 <template>
-  <button class="sk-button">
+  <button :class="['sk-button', type]">
     <span v-if="$slots.default">
       <slot></slot>
     </span>
@@ -11,7 +11,44 @@ export default {
   name: 'SkButton',
 
   props: {
-
-  }
+    type: {
+      type: String,
+      default: 'default'
+    }
+  },
 }
 </script>
+
+<style lang="scss" scoped>
+.sk-button {
+  width: 80px;
+  height: 32px;
+  outline: none;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+
+  font-size: 14px;
+
+  &.default {
+    border: 1px solid #E2608B;
+    background-color: #fff;
+    color: #E2608B;
+  }
+
+  &.primary {
+    background-color: #E2608B;
+    color: white;
+  }
+
+  &.info {
+    background-color: #BDA097;
+    color: #fff;
+  }
+
+  &.danger {
+    background-color: #903555;
+    color: #fff;
+  }
+}
+</style>
--- samples/app.vue
+++ samples/app.vue
@@ -1,5 +1,15 @@
 <template>
   <div>
-    <sk-button>1</sk-button>
+    <sk-button>默认</sk-button>
+    <sk-button type="primary">主要</sk-button>
+    <sk-button type="info">提示</sk-button>
+    <sk-button type="danger">危险</sk-button>
   </div>
 </template>
+
+
+<style lang="scss" scoped>
+button {
+  margin-right: 12px;
+}
+</style>
