57dba74850120682ec7afa46f9c40abe05fd897a
tflin <tflins@163.com>
2021-04-12 23:42:26 +0800

    fix: 修改 vite 配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  root: resolve(__dirname, './samples'),
+  plugins: [
+    vue({
+      include: [/\.vue$/],
+    })
+  ],
 })
