ec58ae1eb3457cd43d88b114cabf490bfbafe6e3
tflin <tflins@163.com>
2021-04-12 23:43:07 +0800

    feat: 新增按钮组件
    ]

---
+++ packages/button/index.ts
@@ -0,0 +1,8 @@
+import { App } from 'vue'
+import Button from './src/button.vue'
+
+Button.install = (app: App) => {
+  app.component(Button.name, Button)
+}
+
+export default Button
---
+++ packages/button/src/button.vue
@@ -0,0 +1,17 @@
+<template>
+  <button class="sk-button">
+    <span v-if="$slots.default">
+      <slot></slot>
+    </span>
+  </button>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'SkButton',
+
+  props: {
+
+  }
+}
+</script>
