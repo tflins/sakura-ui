a1f27d9e4044fa5040c70073dbfc2965c8a1e253
tflin <tflins@163.com>
2021-04-13 16:37:37 +0800

    refactor: 引入 scss 全局变量

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,19 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   root: resolve(__dirname, './samples'),
+  resolve: {
+    alias: [{ find: '@', replacement: resolve(__dirname, './packages') }]
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "@/styles/const.scss";`
+      }
+    }
+  },
   plugins: [
     vue({
-      include: [/\.vue$/],
+      include: [/\.vue$/]
     })
-  ],
+  ]
 })
