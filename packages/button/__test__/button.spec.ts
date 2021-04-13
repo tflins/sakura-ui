d22dce82beca106c952f2178a9f4106641c75575
tflin <tflins@163.com>
2021-04-13 18:15:53 +0800

    chore: 完善 button 单元测试

--- packages/button/__test__/button.spec.ts
+++ packages/button/__test__/button.spec.ts
@@ -1,9 +1,18 @@
 import { mount } from '@vue/test-utils'
 import Button from '../src/index.vue'
 
-describe('button.vue', () => {
+describe('sk-button', () => {
   it('create', () => {
     const wrapper = mount(Button)
     expect(wrapper.classes()).toContain('sk-button')
+  })
+
+  it('type primary', () => {
+    const wrapper = mount(Button, {
+      props: {
+        type: 'primary'
+      }
+    })
+    expect(wrapper.classes()).toContain('primary')
   })
 })
