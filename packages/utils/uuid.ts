76f922b8301582c53ba1a89ce700e6685dbe0148
tflin <tflins@163.com>
2021-04-27 21:01:56 +0800

    feat: 增加 utils

---
+++ packages/utils/uuid.ts
@@ -0,0 +1,6 @@
+export function uuid(): string {
+  const temp_url = URL.createObjectURL(new Blob())
+  const uuid = temp_url.toString()
+  URL.revokeObjectURL(temp_url)
+  return uuid.substr(uuid.lastIndexOf('/') + 1)
+}
