591dab9613409bc9b5e263ad2eef9d86999083fb
tflin <tflins@163.com>
2021-04-29 21:44:53 +0800

    fix: icon 组件支持 size

--- packages/icon/src/index.vue
+++ packages/icon/src/index.vue
@@ -1,16 +1,31 @@
 <template>
-  <i :class="['sk-icon', `sk-icon-${name}`]"></i>
+  <i :class="['sk-icon', `sk-icon-${name}`]" :style="innerStyle"></i>
 </template>
 
 <script lang="ts">
-import { defineComponent, PropType } from 'vue'
+import { defineComponent, computed } from 'vue'
 import './icon-font/iconfont.css'
 
 export default defineComponent({
   name: 'SkIcon',
 
   props: {
-    name: String
+    name: String,
+    size: Number
+  },
+
+  setup(props) {
+    const { size } = props
+    const innerStyle = computed(() => {
+      if(!size) return
+      return {
+        fontSize: `${size}px`
+      }
+    })
+
+    return {
+      innerStyle
+    }
   }
 })
 </script>
