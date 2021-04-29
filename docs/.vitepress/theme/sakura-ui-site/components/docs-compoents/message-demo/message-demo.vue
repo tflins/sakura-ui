2284ef262276243cc538c9b99e3096ceb46bc57c
tflin <tflins@163.com>
2021-04-29 21:45:09 +0800

    docs: 完善 message 组件文档

--- docs/.vitepress/theme/sakura-ui-site/components/docs-compoents/message-demo/message-demo.vue
+++ docs/.vitepress/theme/sakura-ui-site/components/docs-compoents/message-demo/message-demo.vue
@@ -1,6 +1,15 @@
 <template>
-  <div class="message-demo">
-    <sk-button @click="showMessage">消息</sk-button>
+  <div v-if="demoId === '1'" class="message-demo">
+    <sk-button type="success" @click="showMessage1">成功提示</sk-button>
+    <sk-button type="danger" @click="showMessage2">错误提示</sk-button>
+    <sk-button type="info" @click="showMessage3">无类型提示</sk-button>
+    <sk-button type="warning" @click="showMessage4">错误提示</sk-button>
+  </div>
+
+  <div v-if="demoId === '2'" class="message-demo">
+    <sk-button @click="showMessage5">显示 5 秒</sk-button>
+    <sk-button @click="showMessage6">显示 2 秒</sk-button>
+    <sk-button @click="showMessage7">不会自动关闭</sk-button>
   </div>
 </template>
 
@@ -10,13 +19,50 @@ import { Message } from '@/main'
 export default {
   name: 'MessageDemo',
 
+  props: {
+    demoId: {
+      type: String,
+      default: '1'
+    }
+  },
+
   setup() {
-    const showMessage = () => {
-      Message.success('某座天桥 某一张合照')
+    const showMessage1 = () => {
+      Message.success('这是一条成功类型的提示')
+    }
+    const showMessage2 = () => {
+      Message.error('这是一条错误类型的提示')
+    }
+    const showMessage3 = () => {
+      Message.info('这是一条提示类型的提示')
+    }
+    const showMessage4 = () => {
+      Message.warning('这是一条警告类型的提示')
+    }
+    const showMessage5 = () => {
+      Message.success('显示 5 秒', {
+        duration: 5000
+      })
+    }
+    const showMessage6 = () => {
+      Message.success('显示 2 秒', {
+        duration: 2000
+      })
+    }
+    const showMessage7 = () => {
+      Message.success('这个一条不会自动关闭的消息', {
+        duration: 0
+      })
     }
 
     return {
-      showMessage
+      showMessage1,
+      showMessage2,
+      showMessage3,
+      showMessage4,
+      showMessage5,
+      showMessage6,
+      showMessage7
     }
   }
 }
@@ -25,5 +71,9 @@ export default {
 <style lang="scss" scoped>
 .message-demo {
   margin: 16px 0;
+
+  :not(:last-child) {
+    margin-right: 12px;
+  }
 }
-</style>>
+</style>
--- docs/component/message.md
+++ docs/component/message.md
@@ -1,5 +1,11 @@
 # Message 消息
 
+消息组件提供不同类型的消息通知，以此来提示用户。
+
+## 基本用法
+
+从 `sakura-ui` 中引入 `Message` 组件，调用相应的方法使用。
+
 <div class="docs-preview-part">
   <message-demo></message-demo>
 </div>
@@ -18,14 +24,35 @@ export default {
   name: 'MessageDemo',
 
   setup() {
-    const showMessage = () => {
-      Message.success('某座天桥 某一张合照')
+    const showMessage1 = () => {
+      Message.success('这是一条成功类型的提示')
+    }
+    const showMessage2 = () => {
+      Message.error('这是一条错误类型的提示')
+    }
+    const showMessage3 = () => {
+      Message.info('这是一条提示类型的提示')
+    }
+    const showMessage4 = () => {
+      Message.warning('这是一条警告类型的提示')
     }
 
     return {
-      showMessage
+      showMessage1,
+      showMessage2,
+      showMessage3,
+      showMessage4
     }
   }
 }
 </script>
 ```
+
+## 显示时间
+
+通过设置 `duration` 属性，可以控制消息组件的显示时间。
+
+<div class="docs-preview-part">
+  <message-demo demo-id="2"></message-demo>
+</div>
+
